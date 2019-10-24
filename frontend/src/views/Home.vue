e6583bd75e209221b5fb926b61f72e02127e2158
mtf <mtf812@outlook.com>
2019-10-24 20:22:31 +0800

    change logo

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/logo.jpg" />
+    <HelloWorld msg="頑張る！" />
   </div>
 </template>
 
